423aa45d318a39b407ce38cc2543505cf68f3e34
Milos Prchlik <happz@happz.cz>
2012-11-29 00:42:55 +0100

    Fixed hlib.env functionality
    >>>
    >>> import hlib.env
    >>> import hruntime
    >>> ENV = hlib.env.Env('../conf/settlers.conf')
    >>> hruntime.dbroot
    <lib.datalayer.Root object at 0x1a5a758>
    >>>
    ... profit ...
    >>> ENV.stop()
    >>>

--- src/env.py
+++ src/env.py
@@ -17,40 +17,69 @@ import hlib.handlers.root
 import hlib.log.channels.stderr
 import hlib.server
 
-def init_env(config_file):
-  """
-  Read specified configuration file and create and init database access.
+config_defaults = {
+  'server':			{
+    'host':			'127.0.0.1',
+    'port':			8082,
+  },
+  'log':                        {
+    'access_format':		'{date} {time} - {tid} - {request_line} - {response_status} {response_length} - {request_ip} {request_user}'
+  },
+  'session':			{
+    'time':			21600
+  }
+}
 
-  @type config_file:            string
-  @param config_file:           Path to configuration file to be read
-  """
+class EnvEngine(hlib.engine.Engine):
+  def start(self):
+    self.init_stats()
 
-  config = ConfigParser.ConfigParser()
-  config.read(config_file)
+    hlib.event.trigger('engine.Started', None, post = False)
 
-  stderr = hlib.log.channels.stderr.Channel()
+    for s in self.servers:
+      s.start()
 
-  # pylint: disable-msg=E1101
-  hlib.config['log.channels.error'] = stderr
+class Env(object):
+  def __init__(self, config_file):
+    super(Env, self).__init__()
 
-  db_address = hlib.database.DBAddress(config.get('database', 'address'))
-  db = hlib.database.DB('main db', db_address)
-  db.open()
+    config = hlib.ConfigFile(default = config_defaults)
+    config.read(config_file)
 
-  app_config                    = hlib.engine.Application.default_config(config.get('server', 'path'))
-  app_config['title']           = 'hlib - env'
-  app_config['cache.enabled']   = False
+    stderr = hlib.log.channels.stderr.Channel()
 
-  app = hlib.engine.Application('hlib', hlib.handlers.root.Handler(), db, app_config)
-  app.channels.access = [stderr]
-  app.channels.error  = [stderr]
+    # pylint: disable-msg=E1101
+    hlib.config['log.channels.error'] = stderr
 
-  server_config                 = hlib.server.Server.default_config()
-  server_config['host']            = config.get('server', 'host')
-  server_config['port']            = int(config.get('server', 'port'))
-  server_config['max_threads']	= 1
-  server_config['app']             = app
+    db_address = hlib.database.DBAddress(config.get('database', 'address'))
+    db = hlib.database.DB('main db', db_address)
+    db.open()
 
-  server = hlib.server.Server(server_config, (server_config['host'], server_config['port']), hlib.server.RequestHandler)
+    app_config                    = hlib.engine.Application.default_config(config.get('server', 'path'))
+    app_config['title']           = 'hlib - env'
+    app_config['cache.enabled']   = False
 
-  hlib.event.trigger('engine.ThreadStarted', None, server = server)
+    app = hlib.engine.Application('hlib', hlib.handlers.root.Handler(), db, app_config)
+    app.channels.access = [stderr]
+    app.channels.error  = [stderr]
+
+    app.sessions = hlib.http.session.MemoryStorage(app)
+    app.config['sessions.time']           = config.get('session', 'time')
+    app.config['sessions.cookie_name']    = config.get('session', 'cookie_name')
+
+    server_config                 = hlib.server.Server.default_config()
+    server_config['host']            = config.get('server', 'host')
+    server_config['port']            = int(config.get('server', 'port'))
+    server_config['max_threads']	= 1
+    server_config['app']             = app
+
+    self.engine = EnvEngine([server_config])
+    self.engine.start()
+
+    hlib.event.trigger('engine.ThreadStarted', None, server = self.engine.servers[0])
+
+  def quit(self):
+    for server in self.engine.servers:
+      server.stop()
+
+    hlib.event.trigger('engine.Halted', None)
