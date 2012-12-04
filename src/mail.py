8dfb31a793512c0caec5c3db5667d621c9ee1f73
Milos Prchlik <happz@happz.cz>
2012-12-04 10:57:24 +0100

    Working mail - localhost SMTP only

--- src/mail.py
+++ src/mail.py
@@ -9,8 +9,9 @@ __license__             = 'http://www.php-suit.com/dpl'
 
 import smtplib
 import email
-import email
-import email.Utils
+import email.message
+import email.utils
+import email.mime.text
 
 import hlib
 
@@ -31,21 +32,21 @@ def send_email(app, sender, recipient, subject, body):
     else:
       break
 
-  sender_name, sender_addr = parseaddr(sender)
-  recipient_name, recipient_addr = parseaddr(recipient)
+  sender_name, sender_addr = email.utils.parseaddr(sender)
+  recipient_name, recipient_addr = email.utils.parseaddr(recipient)
 
-  sender_name = str(email.Header(unicode(sender_name), header_charset))
-  recipient_name = str(email.Header(unicode(recipient_name), header_charset))
+  sender_name = str(email.Header.Header(unicode(sender_name), header_charset))
+  recipient_name = str(email.Header.Header(unicode(recipient_name), header_charset))
 
   sender_addr = sender_addr.encode('ascii')
   recipient_addr = recipient_addr.encode('ascii')
 
   # pylint: disable-msg=W0631
-  msg = email.MIMEText(body.encode(body_charset), 'plain', body_charset)
-  msg['From'] = email.Utils.formataddr((sender_name, sender_addr))
-  msg['To'] = email.Utils.formataddr((recipient_name, recipient_addr))
-  msg['Subject'] = email.Header(unicode(subject), header_charset)
+  msg = email.mime.text.MIMEText(body.encode(body_charset), 'plain', body_charset)
+  msg['From'] = email.utils.formataddr((sender_name, sender_addr))
+  msg['To'] = email.utils.formataddr((recipient_name, recipient_addr))
+  msg['Subject'] = email.Header.Header(unicode(subject), header_charset)
 
-  smtp = smtplib.SMTP(app.config['mail.server'])
+  smtp = smtplib.SMTP('localhost')
   smtp.sendmail(sender, recipient, msg.as_string())
   smtp.quit()
